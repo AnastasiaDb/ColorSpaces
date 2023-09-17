abad709e0a4fdca74a9ee8871abd969e14bdb391
Anastasia <82052788+AnastasiaDb@users.noreply.github.com>
2023-09-17 17:31:20 +0300

    Реализовала два варианта формулы с учетом разных вкладов R, G и B в интенсивность (оно работает пока что чуть неверно)

--- ColorSpaces/Form1.cs
+++ ColorSpaces/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ColorSpaces
 {
@@ -17,9 +18,65 @@ namespace ColorSpaces
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
 
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedImagePath = openFileDialog.FileName;
+                System.Drawing.Image selectedImage = System.Drawing.Image.FromFile(selectedImagePath);
+
+                // Отображаем выбранное изображение в PictureBox
+                pictureBox1.Image = selectedImage;
+            }
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (pictureBox1.Image == null)
+                MessageBox.Show("Выберите картинку!");
+            else DrawTask1();
+        }
+
+        private void DrawTask1()
+        {
+            Bitmap originalImage = new Bitmap(pictureBox1.Image);
+
+            // Создаем новый Bitmap для grayscale изображения
+            Bitmap newImage1 = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            Bitmap newImage2 = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                for (int j = 0; j < pictureBox1.Height; j++)
+                {
+                    Color pixel = originalImage.GetPixel(i, j);
+                    int newColor = (int)(0.299 * pixel.R + 0.587 * pixel.G + 0.114 * pixel.B);
+                    Color newPixel = Color.FromArgb(newColor, newColor, newColor);
+                    newImage1.SetPixel(i, j, newPixel);
+
+                }
+
+            }
+
+            for (int i = 0; i < pictureBox1.Width; i++)
+            {
+                for (int j = 0; j < pictureBox1.Height; j++)
+                {
+                    Color pixel = originalImage.GetPixel(i, j);
+                    int newColor = (int)(0.2126 * pixel.R + 0.7152 * pixel.G + 0.0722 * pixel.B);
+                    Color newPixel = Color.FromArgb(newColor,newColor,newColor);
+                    newImage2.SetPixel(i, j, newPixel);
+
+                }
+
+            }
+
+            pictureBox2.Image= newImage1;
+            pictureBox3.Image= newImage2;
+        }
+
     }
 }
